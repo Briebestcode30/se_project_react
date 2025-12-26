fe4ef64a997bcf5bc79e7520a737d7df9c96c96c
Briebestcode30 <brieanaharris033@penguin.com>
2025-12-26 17:44:30 -0500

    created new files in components created normalize.css in vendor

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+        Hello Brie
+      </div>
+    </div>
+  );
+}
+
+export default App;
