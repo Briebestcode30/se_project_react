6e3b4e34fc914e9ee3d712f09e7ee4c9b31b69c3
Briebestcode30 <brieanaharris033@penguin.com>
2026-01-04 00:05:32 -0500

    added styling to page imported coorect tags in all files

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,17 @@
+import "./WeatherCard.css";
+import sunny from "../../assets/sunny.svg";
+
+function WeatherCard({ weatherData }) {
+  if (!weatherData?.temp) {
+    return null; // or "Loading..."
+  }
+
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <img src={sunny} alt="sunny" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
